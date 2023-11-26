beea8e536ec294bc74c98902962d73081e1b2f19
radiaide <test@gmail.com>
2023-11-26 14:16:34 +0100

    affichage des projets

--- ProductOwner/Projet.php
+++ ProductOwner/Projet.php
@@ -1,3 +1,17 @@
+<?php
+include("../Connexion.php");
+session_start();
+$id_utilisateur = $_SESSION['utilisateur']['id'];
+
+$sql = "SELECT  projet.nom as nom_projet, description, projet.date_creation as date_creation, date_limite, projet.statut  as statut,equipe.nom AS nom_equipe
+, utilisateur.nom as nom_utilisateur FROM `projet` join utilisateur on utilisateur.id= projet.id_user JOIN
+equipe ON projet.id = equipe.id_projet";
+$requete = $conn->prepare($sql);
+$requete->execute();
+$resultat = $requete->get_result();
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -85,8 +99,7 @@
             <div class="container mx-auto p-6">
                 <h1 class="text-3xl text-center font-bold text-gray-800 mb-6">Project Management</h1>
                 <div class="mb-6">
-                    <button
-                        class="inline-flex items-center text-gray-500 bg-white border border-gray-300
+                    <button class="inline-flex items-center text-gray-500 bg-white border border-gray-300
                                 hover:bg-gray-100  font-medium
                                 rounded-lg text-sm px-3 py-1.5 ">
                         Ajouter un Projet
@@ -99,39 +112,50 @@
                             <tr>
                                 <th scope="col" class="px-6 py-3">Nom du Projet</th>
                                 <th scope="col" class="px-6 py-3">Description</th>
-                                <th scope="col" class="px-6 py-3">Date de Création</th>
-                                <th scope="col" class="px-6 py-3">Deadline</th>
                                 <th scope="col" class="px-6 py-3">Scrum Master</th>
                                 <th scope="col" class="px-6 py-3">Équipe</th>
+                                <th scope="col" class="px-6 py-3">Statut</th>
+                                <th scope="col" class="px-6 py-3">Date de Création</th>
+                                <th scope="col" class="px-6 py-3">Deadline</th>
                                 <th scope="col" class="px-6 py-3">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50
-                                dark:hover:bg-gray-600 ">
-                                <td scope="row" class=" px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                    Projet A</td>
-                                <td class="px-6 py-4">Description du Projet A</td>
-                                <td class="px-6 py-4">2023-01-01</td>
-                                <td class="px-6 py-4">2023-12-31</td>
-                                <td class="px-6 py-4">
-                                    <div class="flex gap-6 items-center"><img src="../Images/user.png" alt=""
-                                            width="40">John Doe
-                                </td>
-                                <td class="py-2 px-4 border-b">Équipe A, Équipe B</td>
-                                <td class="px-6 py-4">
-                                    <div class=" flex gap-6"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                            class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                        </svg><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                        </svg></div>
-                                </td>
-                            </tr>
+
+                            <?php
+                            while ($row = $resultat->fetch_assoc()) {
+
+                                echo " 
+                                    <tr class=\"bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50
+                                    dark:hover:bg-gray-600 \">
+                                    <td scope=\"row\" class=\" px-6 py-4 text-gray-900 whitespace-nowrap
+                                        dark:text-white\">{$row['nom_projet']}</td>
+                                    <td class=\"py-2 px-4 border-b\">{$row['description']}</td>
+                                    <td class=\"py-2 px-4 border-b\">{$row['nom_utilisateur']}</td>
+
+                                    <td class=\"py-2 px-4 border-b\">{$row['nom_equipe']}</td>
+
+                                    <td class=\"py-2 px-4 border-b\">{$row['statut']}</td>
+
+                                    <td class=\"px-6 py-4 border-b\">{$row['date_creation']}</td>
+                                    <td class=\"px-6 py-4 border-b\">{$row['date_limite']}</td>
+                                    <td class=\"px-6 py-4\">
+                                                                    <div class=\" flex gap-6\"><svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\"
+                                                                            viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\"
+                                                                            class=\"w-6 h-6\">
+                                                                            <path stroke-linecap=\"round\" stroke-linejoin=\"round\"
+                                                                                d=\"M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10\" />
+                                                                        </svg><svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\"
+                                                                            stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\">
+                                                                            <path stroke-linecap=\"round\" stroke-linejoin=\"round\"
+                                                                                d=\"M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0\" />
+                                                                        </svg></div>
+                                                                </td>
+                                    </tr>
+                                    ";
+                            }
+                            ?>
+
                         </tbody>
                     </table>
                 </div>
